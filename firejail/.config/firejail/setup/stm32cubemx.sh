1057124f837d0d3b58390afac0f432b332b3210f
mid-kid <esteve.varela@gmail.com>
2021-02-27 23:44:38 +0100

    Miscellaneous changes

--- firejail/.config/firejail/setup/stm32cubemx.sh
+++ firejail/.config/firejail/setup/stm32cubemx.sh
@@ -8,9 +8,9 @@ fetch() {
     trap "rm -rf '$tmp'" EXIT
 
     cd "$tmp"
-    wget 'https://sw-center.st.com/packs/resource/library/stm32cube_mx_v601.zip'
+    wget 'https://sw-center.st.com/packs/resource/library/stm32cube_mx_v611.zip'
     mkdir -p "$prefix"
-    unzip 'stm32cube_mx_v601.zip' -d "$prefix"
+    unzip 'stm32cube_mx_v611.zip' -d "$prefix"
     mv "$prefix/STM32CubeMX.exe" "$prefix/STM32CubeMX"
 }
 
--- firejail/.config/firejail/stm32cubemx.profile
+++ firejail/.config/firejail/stm32cubemx.profile
@@ -2,10 +2,6 @@ whitelist ~/.local/share/firejail/stm32cubemx
 
 mkdir ~/.stm32cubemx
 whitelist ~/.stm32cubemx
-mkdir ~/.stmcube
-whitelist ~/.stmcube
-mkdir ~/.stmcufinder
-whitelist ~/.stmcufinder
 
 ignore net none
 include ~/.config/firejail/inc/java.inc
