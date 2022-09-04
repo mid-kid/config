cce508ac6670d7d2f7343914971ff85ac28dcc36
mid-kid <esteve.varela@gmail.com>
2022-09-04 14:45:14 +0200

    Pre-firefox upgrade config

--- zzz-firefox/user.js
+++ zzz-firefox/user.js
@@ -1,24 +1,20 @@
 /*** [SECTION XXXX]: MY SETTINGS ***/
 
-// Settings to consider
-// browser.cache.disk.enable - keeping sites locally can speed shit up on boot
-// Left off at: dom.push.enabled
-
 user_pref("accessibility.typeaheadfind", false);  // Don't search text when typing in a page
 user_pref("browser.download.useDownloadDir", true);  // Download to the "downloads" folder by default
 user_pref("browser.startup.page", 3);  // Keep session across shutdowns
-user_pref("dom.security.https_only_mode", false);  // Don't warn when a site only supports http
+user_pref("browser.tabs.drawInTitlebar", false);  // Use native title bar, don't have spacer next to tabs
 user_pref("extensions.pocket.enabled", false);  // 3rd party extension, proprietary
 user_pref("general.smoothScroll", false);  // I prefer quick/fast scrolling
 user_pref("keyword.enabled", true);  // Enable search in the URL bar
 user_pref("layout.spellcheckDefault", 0);  // Disable spellchecking
 user_pref("network.dns.disableIPv6", false);  // This is fucking retarded
 user_pref("network.trr.mode", 5);  // Applications should stop trying to avoid the system's DNS resolver
+user_pref("places.history.expiration.max_pages", 1000000000);  // Keep history FOREVER!
 user_pref("privacy.resistFingerprinting.letterboxing", false);  // Don't mess with the content size
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);  // Don't clear everything on shutdown
 user_pref("signon.rememberSignons", false);  // Don't ask to save passwords
 user_pref("widget.non-native-theme.enabled", false);  // I prefer native widgets...
-user_pref("browser.tabs.drawInTitlebar", false);  // Use native title bar, don't have spacer next to tabs
 
 // Disable "handholding" security
 // I know what I install/browse, and I don't want a 3rd party to decide for me
@@ -30,8 +26,12 @@ user_pref("xpinstall.signatures.required", false);  // Disable extension signing
 user_pref("xpinstall.whitelist.required", false);  // Disable extension whitelist
 
 // Try not breaking sites
-user_pref("network.http.referer.XOriginPolicy", 0);
-//user_pref("network.http.referer.XOriginTrimmingPolicy", 0);
-//user_pref("privacy.firstparty.isolate", false);
+user_pref("dom.security.https_only_mode", false);  // Don't warn when a site only supports http
+user_pref("network.http.referer.XOriginPolicy", 0);  // Unfortunately, some sites still require the referral header
+user_pref("security.ssl.require_safe_negotiation", false);  // Not any worse than allowing HTTP connections, padlock shows as broken anyway
+
+// Relly big and leaky browser features
+//user_pref("media.peerconnection.enabled", true);  // WebRTC
+//user_pref("webgl.disabled", false);  // WebGL
 
 user_pref("_user.js.parrot", "SUCCESS");
