cd94b04ff179681df269c130c3fb52545852feea
mid-kid <esteve.varela@gmail.com>
2026-02-18 19:28:26 +0100

    zzz-firefox: Only use posix shell facilities in mozlz4.sh

--- zzz-firefox/mozlz4.sh
+++ zzz-firefox/mozlz4.sh
@@ -5,17 +5,14 @@ set -eu
 # Yes, I wrote a non-compressor in posix shell for this
 
 len=$(wc -c < "$1")
-buf=$(test $len -lt 15 && printf %x0 $len ||
-    rep=$(( ($len - 15) / 255 ))
-    rem=$(( ($len - 15) % 255 ))
-    printf f0
-    seq $rep | xargs -r printf ff%.0s
-    printf %02x $rem
+buf=$(test $len -lt 15 && printf \\%03o $(( len * 16 )) ||
+    rep=$(( (len - 15) / 255 ))
+    rem=$(( (len - 15) % 255 ))
+    printf \\%03o 0xf0
+    test $rep -gt 0 && printf \\377%.0s $(seq $rep)
+    printf \\%03o $rem
 )
-len=$(( $len + $(printf %s $buf | wc -c) / 2 ))
-printf mozLz40\\0
-{
-printf %08x $len | sed 's/../&\n/g' | tac | tr -d \\n
-printf %s $buf
-} | xxd -r -p
+len=$(( len + $(printf $buf | wc -c) ))
+lendat=$(printf \\%03o $(printf %08x $len | sed 's/../0x&\n/g' | tac))
+printf mozLz40\\0$lendat$buf
 cat "$1"
